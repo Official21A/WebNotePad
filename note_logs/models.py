e53a3b098a59808f03c42d51e34d350421edbdc3
Official21A <officialamirhossein21@gmail.com>
2020-09-19 12:39:23 +0430

    Classes add

--- note_logs/models.py
+++ note_logs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notepad(models.Model):
+    # the notepad class which contains notes
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # a method for django to show the users the topic
+        return self.text
+        
+
+class Note(models.Model):
+    # the note class where the user enters its notes
+    notepad = models.ForeignKey(Notepad, on_delete=models.CASCADE)
+    # above var is for database to connect each Note to its own Notepad
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+    	# this inner class will let us add more details about a class for django
+    	verbose_name_plural = 'notes'
+
+    def __str__(self):
+    	# a method for django to show the users the topic
+    	return f"{self.text[:50]}..."	
+        		        
