b50727f57b0c4522394d0d475c48b61bb4116b9b
Official21A <officialamirhossein21@gmail.com>
2020-09-20 12:42:02 +0430

    write home page view

---
+++ note_logs/urls.py
@@ -0,0 +1,17 @@
+# creating the url patterns for model
+
+from django.urls import path
+
+from . import views 
+
+
+app_name = 'note_logs' # this naming helps django to identify this file
+
+urlpatterns = [
+	# Home page
+	path('', views.index, name='index'),
+		#	this will call the index method in views.py to
+		#	show the home page.
+		#	The name is a pattern for the url, so at anytime
+		#	we can refer to it by its name. 
+]
--- note_logs/views.py
+++ note_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	# the function to manage the home page of the web project
+	return render(request, 'note_logs/index.html') # the name of the template
