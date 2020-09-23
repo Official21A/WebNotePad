a5f2927969348339538fa6522125e1b3926e71a9
Official21A <officialamirhossein21@gmail.com>
2020-09-23 15:54:12 +0330

    Migrate database

--- note_logs/models.py
+++ note_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,9 @@ class Notepad(models.Model):
     # the notepad class which contains notes
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # above var shows each notepad for 
+    # a user so just that user can get access to it.
 
     def __str__(self):
         # a method for django to show the users the topic
